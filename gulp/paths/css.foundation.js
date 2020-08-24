0a5d1a17479dbb6049ad2e206a08411ee360f96c
odinokun <odinokun@gmail.com>
2020-08-24 09:11:58 +0300

    include fancybox

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -3,11 +3,9 @@
 module.exports = [
   './node_modules/normalize.css/normalize.css',
   './node_modules/slick-carousel/slick/slick.css',
-  './node_modules/animate.css/animate.min.css'
+  './node_modules/animate.css/animate.min.css',
+  './node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
 
   // include jQuery UI
   // './src/vendors/jquery-ui/jquery-ui.min.css',
-
-  // include fancybox3
-  // './node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
 ];
