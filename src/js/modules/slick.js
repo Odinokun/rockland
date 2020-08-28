c890a0de27d652a43fbf5de6c38bd72da2c8351d
odinokun <odinokun@gmail.com>
2020-08-28 23:01:34 +0300

    did slider

--- src/js/modules/slick.js
+++ src/js/modules/slick.js
@@ -65,21 +65,40 @@ module.exports = function() {
     infinite: true,
     autoplay: false,
     fade: true,
+    speed: 1,
     prevArrow: "<div class='prev'><svg width=\"20\" height=\"33\" viewBox=\"0 0 20 33\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M19 1L1 16.5L19 32\"/></svg></div>",
     nextArrow: "<div class='next'><svg width=\"20\" height=\"33\" viewBox=\"0 0 20 33\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M1 32L19 16.5L1 0.999999\"/></svg></div>"
   });
 
+  let img = $('#move-field__img');
+  //array with offset coordinates
+  let arrTransformImg =      ['',
+    '90%, 90%', '70%, 90%', '50%, 90%', '30%, 90%', '10%, 90%', '-10%, 90%', '-30%, 90%', '-50%, 90%', '-70%, 90%', '-90%, 90%',
+    '90%, 70%', '70%, 70%', '50%, 70%', '30%, 70%', '10%, 70%', '-10%, 70%', '-30%, 70%', '-50%, 70%', '-70%, 70%', '-90%, 70%',
+    '90%, 50%', '70%, 50%', '50%, 50%', '30%, 50%', '10%, 50%', '-10%, 50%', '-30%, 50%', '-50%, 50%', '-70%, 50%', '-90%, 50%',
+    '90%, 30%', '70%, 30%', '50%, 30%', '30%, 30%', '10%, 30%', '-10%, 30%', '-30%, 30%', '-50%, 30%', '-70%, 30%', '-90%, 30%',
+    '90%, 10%', '70%, 10%', '50%, 10%', '30%, 10%', '10%, 10%', '-10%, 10%', '-30%, 10%', '-50%, 10%', '-70%, 10%', '-90%, 10%',
+    '90%,-10%', '70%,-10%', '50%,-10%', '30%,-10%', '10%,-10%', '-10%,-10%', '-30%,-10%', '-50%,-10%', '-70%,-10%', '-90%,-10%',
+    '90%,-30%', '70%,-30%', '50%,-30%', '30%,-30%', '10%,-30%', '-10%,-30%', '-30%,-30%', '-50%,-30%', '-70%,-30%', '-90%,-30%',
+    '90%,-50%', '70%,-50%', '50%,-50%', '30%,-50%', '10%,-50%', '-10%,-50%', '-30%,-50%', '-50%,-50%', '-70%,-50%', '-90%,-50%',
+    '90%,-70%', '70%,-70%', '50%,-70%', '30%,-70%', '10%,-70%', '-10%,-70%', '-30%,-70%', '-50%,-70%', '-70%,-70%', '-90%,-70%',
+    '90%,-90%', '70%,-90%', '50%,-90%', '30%,-90%', '10%,-90%', '-10%,-90%', '-30%,-90%', '-50%,-90%', '-70%,-90%', '-90%,-90%'
+  ];
 
+  //array of selected squares
+  let selectedSquare = ['', 15, 55, 72, 99];
 
-  let arrScale =      [22, 32, 50];
-  let arrTransformX = [23, 33, 51];
-  let arrTransformY = [24, 34, 52];
-
-  // console.log(param);
   $('#cosmetics-slider__list').on('afterChange', function(event, slick, currentSlide, nextSlide){
-    // console.log('arr = ' + arr[slick.currentSlide]);
-    // console.log(slick.currentSlide);
-    console.log('obj = ' + arrScale[currentSlide]);
+    let square = slick.currentSlide; //number of current slide
+
+    if(square === 0) {
+      //conditions for the first slide
+      $(img).css({ transform: 'translate(0, 0) scale(1)'});
+    }else {
+      //current square number
+      let selectPoint = selectedSquare[square];
+      $(img).css({ transform: 'translate(' + arrTransformImg[selectPoint] + ') scale(2)'});
+    }
   });
   // end slickSlider from cosmetic page
 
